dfca51e483c85ce90b2760afab2e1dddf7eaf6dd
Raniery <102702376+Ranieeery@users.noreply.github.com>
2023-04-29 23:34:06 -0300

    Update config.sh

--- linux/config.sh
+++ linux/config.sh
@@ -3,13 +3,25 @@
 # Instalação de dependências
 sudo apt update
 
+# Configuração do Git
+git config --global user.name "Ranieeery"
+git config --global user.email "raniery2003@hotmail.com"
+git config --global init.defaultBranch main
+
+# Instalação do Grub
+sudo apt-get install grub2
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+sudo apt-get update
+sudo apt-get install grub-customizer
+sudo update-grub
+
 # Instalar programas externos .deb
 mkdir -p ~/Downloads/Programas
 cd ~/Downloads/Programas
 sudo dpkg -i *.deb
 
 # Instalar o snapd
-sudo apt install snapd
+sudo apt install -y snapd
 
 # Instalar Spotify, Flatpak e Discord via Flatpak
 sudo snap install spotify
@@ -37,7 +49,14 @@ npm install --global yarn
 
 # Instalar Zsh e Oh My Zsh
 sudo apt install zsh
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" 
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo 'Set ZSH_THEME="powerlevel10k/powerlevel10k" in ~/.zshrc, and source ~/.zshrc'
+
+# Continuação Grub
+echo "sudo nano /etc/default/grub and set GRUB_DISABLE_OS_PROBER=false"
+echo "sudo apt-get install os-prober"
 
 # Limpar travas do APT, se necessário
 # sudo rm /var/lib/dpkg/lock-frontend

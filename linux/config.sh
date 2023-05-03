9fb79cbc0881e382716d9cd4b996caa8fdc23067
Raniery <102702376+Ranieeery@users.noreply.github.com>
2023-05-03 15:27:33 -0300

    Update config.sh

--- linux/config.sh
+++ linux/config.sh
@@ -51,6 +51,34 @@ sudo npm install -g n
 sudo n lts
 npm install --global yarn
 
+# Instalar Docker
+sudo apt remove docker-desktop
+rm -r $HOME/.docker/desktop
+sudo rm /usr/local/bin/com.docker.cli
+sudo apt purge docker-desktop
+
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg
+    
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker run hello-world
+
+echo "https://docs.docker.com/desktop/install/ubuntu/"
 
 # Instalar Zsh e Oh My Zsh
 sudo apt install zsh -y

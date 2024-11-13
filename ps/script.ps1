51d8fcd3bc1ace080e2bc54980e8c863fdbe996e
Raniery <102702376+Ranieeery@users.noreply.github.com>
2024-11-12 23:53:12 -0300

    feat: update script.ps1

--- ps/script.ps1
+++ ps/script.ps1
@@ -12,6 +12,21 @@ winget install yt-dlp
 yt-dlp --update
 
 # Need to install Node.js (Contains choco)
+# installs fnm (Fast Node Manager)
+winget install Schniz.fnm
+
+# configure fnm environment
+fnm env --use-on-cd | Out-String | Invoke-Expression
+
+# download and install Node.js
+fnm use --install-if-missing 22
+
+# verifies the right Node.js version is in the environment
+node -v # should print `v22.11.0`
+
+# verifies the right npm version is in the environment
+npm -v # should print `10.9.0`
+
 choco upgrade chocolatey
 choco install mpv
 choco update
